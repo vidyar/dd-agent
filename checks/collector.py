eabca105c9e473e0607dcd996d28bba7b555bae5
Joseph Long <joseph@datadoghq.com>
2013-06-05 13:48:01 -0400

    Restore initial work on Nagios new-style check
    Undo commit 98c787292123a1638c18b2cb0452176266f85d88 to restore the
    initial work on the new-style Nagios check as a starting point

--- checks/collector.py
+++ checks/collector.py
@@ -17,7 +17,6 @@ import checks.system.unix as u
 import checks.system.win32 as w32
 from checks.agent_metrics import CollectorMetrics
 from checks.ganglia import Ganglia
-from checks.nagios import Nagios
 from checks.cassandra import Cassandra
 from checks.datadog import Dogstreams, DdForwarder
 from checks.check_status import CheckStatus, CollectorStatus, EmitterStatus
@@ -91,11 +90,6 @@ class Collector(object):
                 log.info("Registered custom check %s" % module_spec)
             except Exception, e:
                 log.exception('Unable to load custom check module %s' % module_spec)
-
-        # Event Checks
-        self._event_checks = [
-            Nagios(get_hostname()),
-        ]
 
         # Resource Checks
         self._resources_checks = [
@@ -212,12 +206,6 @@ class Collector(object):
         # metrics about the forwarder
         if ddforwarderData:
             payload['datadog'] = ddforwarderData
-
-        # Process the event checks. 
-        for event_check in self._event_checks:
-            event_data = event_check.check(log, self.agentConfig)
-            if event_data:
-                events[event_check.key] = event_data
 
         # Resources checks
         if self.os != 'windows':
